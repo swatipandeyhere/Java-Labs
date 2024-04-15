90a71c7dac9c32b27243bf981214da6145aa0ec6
Swati Pandey <swatipandeyhere@gmail.com>
2024-04-15 12:54:03 +0530

    Edit CompoundAmount.java file

--- 2-concept-decision-making-control-structures-switch-case-solution/src/main/java/org/prograd/CompoundAmount.java
+++ 2-concept-decision-making-control-structures-switch-case-solution/src/main/java/org/prograd/CompoundAmount.java
@@ -3,7 +3,7 @@ package org.prograd;
 public class CompoundAmount {
     double compoundAmount;
     public double getCompoundAmount(double principal, int time, double rate) {
-
+        compoundAmount = principal * Math.pow((1 + rate/100), time);
         return compoundAmount;
     }
 }
