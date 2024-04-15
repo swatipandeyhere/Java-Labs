e83197d492ff53dbf6d3b3014622405a9e01cb7e
Swati Pandey <swatipandeyhere@gmail.com>
2024-04-15 12:43:32 +0530

    initial commit

---
+++ 2-concept-decision-making-control-structures-switch-case-boilerplate/src/main/java/org/prograd/CompoundAmount.java
@@ -0,0 +1,9 @@
+package org.prograd;
+
+public class CompoundAmount {
+    double compoundAmount;
+    public double getCompoundAmount(double principal, int time, double rate) {
+
+        return compoundAmount;
+    }
+}
---
+++ 2-concept-decision-making-control-structures-switch-case-boilerplate/src/main/java/org/prograd/Rate.java
@@ -0,0 +1,10 @@
+package org.prograd;
+
+public class Rate {
+    double rate;
+
+    public double getRate(double principal, int time) {
+
+        return rate;
+    }
+}
