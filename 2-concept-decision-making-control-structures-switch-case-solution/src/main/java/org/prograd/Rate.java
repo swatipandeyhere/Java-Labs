c4520264008afa0ee5e049512175d5c7d6601394
Swati Pandey <swatipandeyhere@gmail.com>
2024-04-15 12:51:38 +0530

    Edit Rate.java file

--- 2-concept-decision-making-control-structures-switch-case-solution/src/main/java/org/prograd/Rate.java
+++ 2-concept-decision-making-control-structures-switch-case-solution/src/main/java/org/prograd/Rate.java
@@ -4,7 +4,27 @@ public class Rate {
     double rate;
 
     public double getRate(double principal, int time) {
-
+        if (principal < 25000) {
+            rate = 6.0;
+        } else {
+            switch (time) {
+                case 7:
+                    rate = 7.0;
+                    break;
+                case 10:
+                    rate = 8.5;
+                    break;
+                case 15:
+                    rate = 9.5;
+                    break;
+                case 20:
+                    rate = 12.5;
+                    break;
+                default:
+                    rate = 6.75;
+                    break;
+            }
+        }
         return rate;
     }
 }
